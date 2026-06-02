a70d86412ae3f0d054f58d888c62a6827496a4f9
나상욱 <garidana@gmail.com>
2026-06-03 02:32:23 +0900

    feat : Boss Behaviour
    보그구현

---
+++ Assets/Scripts/UI/BossHpBarUi.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHpBarUi : MonoBehaviour
+{
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private GameObject root;
+
+    private BossMonster boss;
+
+    private void Awake()
+    {
+        Hide();
+    }
+
+    public void Show(BossMonster targetBoss)
+    {
+        if (targetBoss == null)
+        {
+            return;
+        }
+
+        if (boss != null)
+        {
+            boss.OnHpChanged -= UpdateHp;
+        }
+
+        boss = targetBoss;
+        boss.OnHpChanged += UpdateHp;
+
+        SetVisible(true);
+        UpdateHp(1f);
+    }
+
+    public void Hide()
+    {
+        if (boss != null)
+        {
+            boss.OnHpChanged -= UpdateHp;
+            boss = null;
+        }
+
+        SetVisible(false);
+    }
+
+    private void UpdateHp(float ratio)
+    {
+        if (hpSlider != null)
+        {
+            hpSlider.value = ratio;
+        }
+
+        if (boss != null && boss.IsDead)
+        {
+            Hide();
+        }
+    }
+
+    private void SetVisible(bool visible)
+    {
+        if (root != null)
+        {
+            root.SetActive(visible);
+        }
+        else
+        {
+            gameObject.SetActive(visible);
+        }
+
+        if (hpSlider != null)
+        {
+            hpSlider.gameObject.SetActive(visible);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (boss != null)
+        {
+            boss.OnHpChanged -= UpdateHp;
+        }
+    }
+}
